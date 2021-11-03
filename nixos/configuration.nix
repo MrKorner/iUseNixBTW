5f11e33ac14c10d39dc3943bbe975ce84b437f32
MrKorner <kornerius@protonmail.com>
2021-11-03 14:27:24 +0100

    a commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,25 +1,52 @@
 { config, pkgs, ... }:
-let pkgBin = name: "${pkgs.${name}}/bin/${name}"; in
+
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      <home-manager/nixos>
-    ];
-  nixpkgs.config.allowUnfree = true;
+  imports = [ ./hardware-configuration.nix];
 
-  home-manager = {
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    users.korner = import ./home.nix;
+  #Networking
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
+  networking.interfaces.wlp5s0.useDHCP = true;
+  networking.wireless.iwd.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+  time.timeZone = "Europe/Prague";
+  #Networking
+
+  #Desktop shenanigans
+  services.xserver.enable = true;
+  services.xserver.desktopManager.pantheon.enable = true;
+  programs.pantheon-tweaks.enable = true;
+  services.thermald.enable = true;
+  services.printing.enable = false;
+  services.irqbalance.enable = true;  
+  
+  #Hardware shenanigans
+  services.xserver.videoDrivers = [ "modesetting" "amdgpu" ];
+  zramSwap.enable = true;
+  zramSwap.memoryPercent = 75;
+  security.rtkit.enable = true;
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages = with pkgs; [ vaapiIntel amdvlk intel-compute-runtime rocm-opencl-icd ];
+  hardware.opengl.driSupport = true;
+
+  #Desktop Integrations
+  services.flatpak.enable = true;
+    xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+      ];
+      gtkUsePortal = true;
+    };
   };
+  #Desktop Integrations
 
-  users.users.korner = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "video" "networkmanager" "audio" ];
-  };
-
-  boot.loader = {
+  #Boot shenanigans
+    boot.loader = {
+    systemd-boot.enable = false;
     efi = {
       canTouchEfiVariables = false;
     };
@@ -29,53 +56,10 @@ let pkgBin = name: "${pkgs.${name}}/bin/${name}"; in
       device = "nodev";
     };
   };
-  networking.firewall.enable = false;
-  networking.networkmanager.enable = true;
-  networking.hostName = "nixos";
-  time.timeZone = "Europe/Prague";
+ #Boot shenanigans
 
-  hardware.bluetooth.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
-  hardware.opengl.enable = true;
-
-  programs.light.enable = true;
-  programs.dconf.enable = true;
-  programs.steam.enable = true;
-  programs.java.enable = true;
-  programs.sway = {                                                                                                                                                                                           
-     enable = true;                                                                                                                                                                                               
-     wrapperFeatures.gtk = true;                                                                    
-  };  
-  xdg = {
-    portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-      ];
-      gtkUsePortal = true;
-    };
-  };
-
-  zramSwap.enable = true;
-  zramSwap.memoryPercent = 75;
-  security.rtkit.enable = true;
-  sound.mediaKeys.enable = true;
-
-  services.xserver.videoDrivers = [ "modesetting" "amdgpu" ];
-  services.blueman.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-  services.thermald.enable = true;
-  services.gnome3.gnome-keyring.enable = true;
-
-  systemd.services = {
+ #Service config
+    systemd.services = {
     NetworkManager-wait-online = {
       enable = false;
       restartIfChanged = false;
@@ -90,19 +74,65 @@ let pkgBin = name: "${pkgs.${name}}/bin/${name}"; in
     };
   };
 
-  environment.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";
-  };
-  environment.systemPackages = with pkgs; [
-    mlocate multimc micro pulseaudio
-    noto-fonts-cjk noto-fonts-extra lm_sensors
-    celluloid pfetch htop time gimp gthumb wget
-    firefox-wayland evince transmission teams unrar
-    gnome3.dconf-editor powertop mc cryptsetup git
-    polkit_gnome wirelesstools deadbeef pavucontrol
-    wf-recorder wl-clipboard freetube psmisc lutris                                                                                    
-    swaybg slurp wofi kitty mako grim slurp swaylock
-  ];
+ #Service config 
 
-  system.stateVersion = "20.09";
+
+ #Configure keymap in X11
+ services.xserver.layout = "us";
+ services.xserver.xkbOptions = "eurosign:e";
+
+ #Sound
+ sound.enable = true;
+ sound.mediaKeys.enable = true;
+ hardware.pulseaudio.enable = true;
+ hardware.bluetooth.enable = true;
+ hardware.bluetooth.settings = {
+ General = {
+    Enable = "Source,Sink,Media,Socket";
+    };
+  };
+  #Sound 
+
+  #User options
+  users.users.korner = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "mlocate" "wheel" "audio" "video" ];
+  };
+
+  programs.bash.shellAliases = {
+   l = "ls -CF";
+   dir = "dir --color=auto";
+   vdir = "vdir --color=auto";
+   grep = "grep --color=auto";
+   fgrep = "fgrep --color=auto";
+   egrep = "egrep --color=auto";
+   ll = "ls -lh";
+   ls = "ls --color=auto";
+   flatpak = "flatpak --user";
+ };
+ #User options
+
+ #Packages
+ nixpkgs.config.allowUnfree = true;
+
+ environment.pantheon.excludePackages = with pkgs; [
+ onboard qgnomeplatform epiphany gnome.geary pantheon.switchboard-plug-a11y
+ pantheon.elementary-tasks pantheon.elementary-print-shim pantheon.elementary-onboarding pantheon.elementary-mail
+ pantheon.elementary-feedback pantheon.elementary-capnet-assist pantheon.elementary-camera
+ ];
+ 
+ services.locate = {
+    enable = true;
+    locate = pkgs.mlocate;
+    interval = "hourly";
+ };
+
+ environment.systemPackages = with pkgs; [ 
+ wget noto-fonts-cjk noto-fonts-extra lm_sensors htop time unrar strace mc
+ wgetpaste psmisc lm_sensors cryptsetup powertop tree file git prelink
+ ];
+ #Packages
+
+ system.stateVersion = "21.05";
 }
+
--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,23 +8,28 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b699ccd4-a71d-4100-b2ab-1ed089cbb9fa";
+    { device = "/dev/disk/by-uuid/8bd0a08d-25b3-48b7-8965-ab4c32756c1b";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F8A2-0D4F";
+    { device = "/dev/disk/by-uuid/2636-B7C7";
       fsType = "vfat";
     };
 
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/63c0ab39-27a0-44ec-96ca-20c8a5b2b1aa";
+      fsType = "ext4";
+    };
+
   fileSystems."/mnt/Data" =
-    { device = "/dev/disk/by-uuid/f85d9202-9d9b-4ec2-b2b1-5f212511c7d6";
+    { device = "/dev/sda1";
       fsType = "ext4";
     };
 
