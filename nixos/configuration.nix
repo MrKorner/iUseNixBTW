8f2a87c1885f0814450b6c817d079712c9a9ce53
MrKorner <kornerius@protonmail.com>
2023-04-28 04:26:03 +0200

    ugagaga

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
@@ -8,16 +5,15 @@
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
   nix.settings.extra-experimental-features = "nix-command flakes";
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  
   services.auto-cpufreq.enable = false;
   boot.tmp.cleanOnBoot = true;
   boot.tmp.useTmpfs = true;
@@ -42,7 +38,6 @@
   };
   networking.useDHCP = false;
 
-  # Set your time zone.
   time.timeZone = "Europe/Prague";
 
   programs.bash.promptInit = ''PS1="\[\033[38;5;196m\]\\$\[$(tput sgr0)\][\[$(tput sgr0)\]\[\033[38;5;82m\]\A\[$(tput sgr0)\]]\[$(tput sgr0)\]\[\033[38;5;50m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;199m\]\H\[$(tput sgr0)\]\n\[$(tput sgr0)\]\[\033[38;5;226m\]>\w:\[$(tput sgr0)\]"'';
@@ -59,7 +54,6 @@
     ls = "ls --color=auto";
   };
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "cs_CZ.UTF-8/UTF-8"];
   i18n.extraLocaleSettings = {
@@ -108,7 +102,7 @@
   services.xserver.desktopManager.plasma5.enable = true;
   environment.plasma5.excludePackages = with pkgs.libsForQt5; [
   oxygen khelpcenter plasma-browser-integration print-manager ];
-  # Enable sound with pipewire.
+
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -126,8 +120,6 @@
     interval = "hourly";
   };
 
-  #virtualisation.podman.enable = true;
-  #virtualisation.libvirtd.enable = true;
   zramSwap.enable = true;
   zramSwap.memoryPercent = 50;
   hardware.opengl.enable = true;
@@ -144,18 +136,14 @@
   ];
   xdg.portal.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.korner = {
     isNormalUser = true;
     description = "korner";
     extraGroups = ["networkmanager" "wheel" "video" "input" "plugdev"];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget noto-fonts-cjk lm_sensors htop time unrar gnutar pciutils prismlauncher
     (pkgs.emacsUnstable.override {
@@ -172,5 +160,5 @@
     acpi usbutils wgetpaste psmisc cryptsetup file git mc lsof neofetch librewolf
     ffmpeg-full nheko btrfs-progs noto-fonts-emoji-blob-bin bc freetube inkscape nicotine-plus
   ];
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "22.11";
 }
